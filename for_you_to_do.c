00293f2c68272b59a8062abdaa1472f0daf91632
LinqingLi <62589604+Lanceart@users.noreply.github.com>
2022-10-22 21:41:58 -0700

    final?s

--- for_you_to_do.c
+++ for_you_to_do.c
@@ -28,7 +28,7 @@ int mydgetrf(double *A, int *ipiv, int n)
     int i, j, k, max_index, tmp2;
     double max, tmp1;
     // used for swapping rows
-    double * tmp_row = (double *)malloc(sizeof(double) * n);
+    double * temps_double = (double *)malloc(sizeof(double) * n);
     
     for (i = 0; i < n; i++)
     {
@@ -55,10 +55,16 @@ int mydgetrf(double *A, int *ipiv, int n)
             ipiv[i] = ipiv[max_index];
             ipiv[max_index] = tmp2;
 		
-            // swap rows of A
-            memcpy(tmp_row, A + i * n, sizeof(double) * n);
-            memcpy(A + i * n, A + max_index * n, sizeof(double) * n);
-            memcpy(A + max_index * n, tmp_row, sizeof(double) * n);
+        //
+        /*
+        memcpy(temps_double, A + max_index * n, sizeof(double) * n);
+            memcpy(A + max_index * n, A + A + i * n, sizeof(double) * n);
+            memcpy(A + A + i * n, temps_double, sizeof(double) * n);
+        */
+            //A + max_index * n
+            memcpy(temps_double, A + max_index * n, sizeof(double) * n);
+            memcpy(A + max_index * n, A + A + i * n, sizeof(double) * n);
+            memcpy(A + A + i * n, temps_double, sizeof(double) * n);
         }
     
         for (j = i + 1; j < n; j++)
@@ -70,43 +76,32 @@ int mydgetrf(double *A, int *ipiv, int n)
             }
         }
     }
-    free(tmp_row);
+    free(temps_double);
     return 0;
 }
 
-/**
- * 
- * this function computes triangular matrix - vector solver
- * for a square matrix . according to lecture slides, this
- * function computes forward AND backward subtitution in the
- * same function.
- * 
- * syntax 
- *  
- *  input :
- *      UPLO  'L' or 'U' , denotes whether input matrix is upper
- *                         lower triangular . ( forward / backward
- *                         substitution )
- * 
- *      A     n by n     , square matrix
- * 
- *      B     1 by n     , vector
- * 
- *      ipiv  1 by n     , vector , denotes interchanged index due
- *                                  to pivoting by mydgetrf()
- * 
- *      n                , length of vector / size of matrix
- *  
- *  output :
- *      none
- * 
- **/
+
 void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
 {
     int i, j;
     double sum = 0;
     double * tmp_B = (double *)malloc(sizeof(double) * n);
     
+    if (UPLO == 'U')
+    {   
+        for (i = n - 1; i >= 0; i--)
+        {
+            sum = 0;
+            int temp_index = i*n;
+            for (j = i + 1; j < n; j++)
+            {
+                sum += B[j] * A[temp_index + j];
+            }
+            B[i] = (B[i] - sum) / A[temp_index + i];
+        }
+    }
+
+
     if (UPLO == 'L')
     {
 	   
@@ -118,26 +113,16 @@ void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
         for (i = 0; i < n; i++)
         {
             sum = tmp_B[i];
+            int temp_index = i*n;
             for (j = 0; j < i; j++)
             {
-                sum -= B[j] * A[i * n + j];
+                sum -= B[j] * A[temp_index + j];
             }
             B[i] = sum;
         }
     }
-    else if (UPLO == 'U')
-    {
-	// Ux = y, backward subtitution     
-        for (i = n - 1; i >= 0; i--)
-        {
-            sum = 0;
-            for (j = i + 1; j < n; j++)
-            {
-                sum += B[j] * A[i * n + j];
-            }
-            B[i] = (B[i] - sum) / A[i * n + i];
-        }
-    }
+    
+    
     free(tmp_B);
     return;
 }
@@ -154,7 +139,6 @@ int get_block_size(){
 //The matrix multiplication function used in blocked GEPP.
 // You need to let the mydgemm adapt to non-square inputs.
 void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b) {
-    /* add your code here */
     /* please just copy from your lab1 function optimal( ... ) */
     /* A, B and C are n x n matrices.
     /* This function computes C[:i,:j]+=A[:i,:k]*B[:k,:j] (the first i rows and k columuns of A multiplies the first k rows and j columuns of B added to the the first i rows and j columuns of C)
@@ -168,36 +152,41 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
     for (i1 = i; i1 < i+b; i1 += 2) {
         for (k1 = k; k1 < k+b; k1 += 2) {
             // reg for A
-            register int ra1 = i1*n + k1;// A[i1, k1]
-            register int ra2 = ra1 + n;// A[(i1+1), k1]
-            register double a00 = A[ra1];// A[i1, k1]
-            register double a01 = A[ra1+1];// A[i1, k1+1]
-            register double a10 = A[ra2];// A[(i1+1), k1]
-            register double a11 = A[ra2+1];// A[(i1+1), (k1+1)]
+            register int t_temp = i1*n + k1;
+            register int q_temp = t_temp + n;
+
+
+            register double A_00 = A[t_temp];
+            register double A_01 = A[t_temp+1];
+            register double A_10 = A[q_temp];
+            register double A_11 = A[q_temp+1];
             for (j1 = j; j1 < j+b; j1 += 2) {
-                // reg for C
-                register int rc1 = i1*n + j1;// C[i1, j1]
-                register int rc2 = rc1 + n;// C[i1+1, j1]
-                register double c00 = C[rc1];// C[i1, j1]
-                register double c01 = C[rc1+1];// C[i1, j1+1]
-                register double c10 = C[rc2];// C[i1+1, j1]
-                register double c11 = C[rc2+1];// C[i1+1, j1+1]
-                // reg for B
-                register int rb1 = k1*n + j1;// B[k1, j1]
-                register int rb2 = rb1 + n;// B[(k1+1), j1]
-                register double b00 = B[rb1];// B[k1, j1]
-                register double b01 = B[rb1+1];// B[k1, j1+1]
-                register double b10 = B[rb2];// B[(k1+1), j1]
-                register double b11 = B[rb2+1];// B[k1+1, j1+1]
-                // calculate C
-                c00 -= a00 * b00 + a01 * b10;
-                c01 -= a00 * b01 + a01 * b11;
-                c10 -= a10 * b00 + a11 * b10;
-                c11 -= a10 * b01 + a11 * b11;
-                C[rc1] = c00;
-                C[rc1+1] = c01;
-                C[rc2] = c10;
-                C[rc2+1] = c11;
+                
+                register int rb1 = k1*n + j1;
+                register int rb2 = rb1 + n;
+                register double B_00 = B[rb1];
+                register double B_01 = B[rb1+1];
+                register double B_10 = B[rb2];
+                register double B_11 = B[rb2+1];
+
+
+
+                register int rc1 = i1*n + j1;
+                register int rc2 = rc1 + n;
+                register double C_00 = C[rc1];
+                register double C_01 = C[rc1+1];
+                register double C_10 = C[rc2];
+                register double C_11 = C[rc2+1];
+
+
+                C_00 =C_00 - ( A_00 * B_00 + A_01 * B_10);
+                C_01 =C_01 (A_00 * B_01 + A_01 * B_11);
+                C_10 =C_10 (A_10 * B_00 + A_11 * B_10);
+                C_11 =C_11 (A_10 * B_01 + A_11 * B_11);
+                C[rc1] = C_00;
+                C[rc1+1] = C_01;
+                C[rc2] = C_10;
+                C[rc2+1] = C_11;
             }
         }
     }
@@ -205,86 +194,69 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
 }
 
 
-/**
- * 
- * this function computes LU decomposition
- * for a square matrix using block gepp introduced in course
- * lecture .
- * 
- * just implement the block algorithm you learned in class.
- * 
- * syntax 
- *  
- *  input :
- *      
- * 
- *      A     n by n     , square matrix
- * 
- *      ipiv  1 by n     , vector , denotes interchanged index due
- *                                  to pivoting by mydgetrf()
- * 
- *      n                , length of vector / size of matrix
- *     
- *      b                , block size   
- *  output :
- *      return -1 : if the matrix A is singular (max pivot == 0)
- *      return  0 : return normally 
- * 
- **/
+
 int mydgetrf_block(double *A, int *ipiv, int n, int b) {
-    int i, j, k;
-    int max_Aii;
-    int ib;
+    double* temps_index_double = (double*)malloc(n * sizeof(double)); 
+    int max_index_me,temp_i,i, j, k;
     double max;
-    double* temp_Aii = (double*)malloc(n * sizeof(double)); // a temp row for swap
     
-    for (ib = 0;ib < n;ib += b) {
-        for (i = ib; i < ib+b; i++) {
+    
+    for (temp_i = 0;temp_i < n;temp_i += b) {
+        //if(temp_i == n)break;
+        for (i = temp_i; i < temp_i+b; i++) {
             max = fabs(A[i*n + i]);
-            max_Aii = i;
+            max_index_me = i;
             for (j = i+1;j < n;j++) {
                 if ( fabs(A[j*n + i]) > max ) {
                     max = fabs(A[j*n + i]);
-                    max_Aii = j; // |A[j,i]| is the biggest in rest of i-th col
+                    max_index_me = j; 
                 }
             }
-            // swap row i and max_Aii and the corrosponding vector
+
             if (max == 0) {
                 return -1;
             } else {
-                if (max_Aii != i) { 
-                    // swap i-th and max_Aii-th element of vector pivot
+                if (max_index_me != i) { 
+
                     int temp = ipiv[i];
-                    ipiv[i] = ipiv[max_Aii];
-                    ipiv[max_Aii] = temp;
-                    // swap i-th row and max_Aii-th row
-                    memcpy(temp_Aii, A + i*n, n*sizeof(double));
-                    memcpy(A + i*n, A + max_Aii*n, n*sizeof(double));
-                    memcpy(A + max_Aii*n, temp_Aii, n*sizeof(double));
+                    ipiv[i] = ipiv[max_index_me];
+                    ipiv[max_index_me] = temp;
+
+                    memcpy(temps_index_double, A + i*n, n*sizeof(double));
+                    memcpy(A + i*n, A + max_index_me*n, n*sizeof(double));
+                    memcpy(A + max_index_me*n, temps_index_double, n*sizeof(double));
+
+                    /*
+                    memcpy(temps_index_double, A + i*n, n*sizeof(double));
+                    memcpy(A + i*n, A + max_index_me*n, n*sizeof(double));
+                    memcpy(A + max_index_me*n, temps_index_double, n*sizeof(double));
+                    */
                 }
             }
+
+            if(max ==0){return -1}
             for (j = i+1; j < n; j++) {
                 A[j*n + i] = A[j*n + i] / A[i*n + i];
-                for (k = i+1; k < ib+b; k++) {
+                for (k = i+1; k < temp_i+b; k++) {
                     A[j*n + k] -= A[j*n + i] * A[i*n + k];
                 }
             }
         }
-        // A(ib:end , end+1:n) = LL-1 * A(ib:end , end+1:n), update next b rows of U
-        for (i = ib; i < ib+b; i++) {
-            for (j = ib+b;j < n;j++) { // j:[end+1=ib+b-1+1, n]
-                double sum = 0.0;
-                for (k = ib;k < i;k++) {
+        double sum = 0.0;
+        for (i = temp_i; i < temp_i+b; i++) {
+            for (j = temp_i+b;j < n;j++) { 
+                sum = 0.0;
+                for (k = temp_i;k < i;k++) {
                     sum += A[i*n + k] * A[k*n + j];
                 }
                 A[i*n + j] -= sum;
             }
         }
 
-        // update A(end+1=ib+b-1+1 : n, end+1:n) block by block
-        for (i = ib+b;i < n;i+=b) {
-            for (j = ib+b;j < n;j+=b) {
-                mydgemm(A, A, A, n, i, j, ib, b);
+
+        for (i = temp_i+b;i < n;i+=b) {
+            for (j = temp_i+b;j < n;j+=b) {
+                mydgemm(A, A, A, n, i, j, temp_i, b);
             }
         }
     }
