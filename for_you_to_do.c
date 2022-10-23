cb07a6f85ec3bec18a088c97d1b237fb47180f14
LinqingLi <62589604+Lanceart@users.noreply.github.com>
2022-10-22 18:14:18 -0700

    final

--- for_you_to_do.c
+++ for_you_to_do.c
@@ -163,7 +163,30 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
     /* In fact this function won't be directly called in the tester code, so you can modify the declaration (parameter list) of mydgemm() if needed. 
     /* you may copy the code from the optimal() function or any of the other functions in your lab1 code (optimized code recommended).*/
     /* add your code here */
-    
+    	int i , j , k , iB, jB , kB;
+        for( k = 0;k < maty ; k += b)
+                for( i = 0;i < matx;i += b)
+                        for( j = 0;j < matx ;j += b)
+                        {
+                                for( kB = k;kB < k + b && kB < maty;kB += 2)
+                                        for( iB = i;iB <i + b && iB < matx;iB += 2)
+                                        {
+                                                register int regA00 = iB *n + kB;
+                                                register int regA10 = regA00 + n;
+                                                register double a00 = A[regA00],a01 = A[regA00 + 1],a10 = A[regA10],a11 = A[regA10 + 1];
+                                                for(jB = j;jB < j + b && jB < matx;jB += 2)
+                                                {
+                                                        register int regB00 = kB * n + jB,regC00 = iB * n + jB;
+                                                        register int regB10 = regB00 + n,regC10 = regC00 + n;
+                                                        register double b00 = B[regB00],b01 = B[regB00 + 1],b10 = B[regB10],b11 = B[regB10 + 1];
+                                                        register double c00 = C[regC00],c01 = C[regC00 + 1],c10 = C[regC10],c11 = C[regC10 + 1];
+                                                        C[regC00] -= a00 * b00 + a01 * b10;
+                                                        C[regC00+1] -= a00 * b01 + a01 * b11;
+                                                        C[regC10] -= a10 * b00 + a11 * b10;
+                                                        C[regC10+1] -= a10 * b01 + a11 * b11;
+                                                }
+                                        }
+                        }
     return;
 }
 
@@ -197,5 +220,73 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
  **/
 int mydgetrf_block(double *A, int *ipiv, int n, int b) 
 {
+    int i, maxind, k, j, ib, end, temps, t;
+		double max;
+		for(ib = 0; ib < n - 1; ib += b)
+		{
+			/*Partial Pivoting*/
+			end = ((n-1) > (ib + b -1)) ? (ib + b - 1) : n-1;
+			//printf("end = %d\n",end);
+			for(i = ib; i <= end; i++)
+			{
+				maxind = i;
+				max = fabs(A[i*n+i]);
+				for(k = i+1; k < n; k++)
+				{
+					if(fabs(A[k * n + i]) > max)
+					{
+						maxind = k;
+						max = fabs(A[k * n + i]);
+					}
+				}
+				if(max == 0) return -1;
+				else if (maxind !=i)
+				{
+
+					/*Save Pivot Infortmation*/
+					temps = ipiv[i];
+					ipiv[i] = ipiv[maxind];
+					ipiv[maxind] = temps;
+					/*Swap rows*/
+					for(j = 0; j < n; j++)
+					{
+						double tempv;
+						tempv = A[i * n + j];
+						A[i * n + j] = A[maxind * n + j];
+						A[maxind * n + j] = tempv;
+					}
+				}
+
+				/*Update columns i+1 to end*/
+				for(j = i + 1; j < n; j++)
+				{
+					A[j * n + i] = (double)A[j * n + i] / A[i * n + i];
+					for(t = i + 1;t <= end; t++)
+					{
+						A[j*n+t] = A[j*n+t] - A[j*n+i] * A[i*n+t];
+					}
+				}
+			}
+
+			/*inv(LL)*/
+			/*double y;y = (double *) malloc(sizeof(double) * (end - ib + 1) * (n - end));y[0] =; */
+			for(i = ib; i <= end; i++)
+			{
+				for(k = end +1; k < n; k++)
+				{
+					double sum = 0;
+					for(j = ib; j < i; j++)
+					{
+						sum += A[i * n + j] * A [j * n + k];
+					}
+					A[i * n + k] -= sum;
+				}
+			}
+			/*Delayed update of rest of matrix using matrix-matrix multiplication*/
+			/*void mydgemm(double *A, double *B, double *C, int n, int matx, int maty, int b)*/
+			//if(end!=n)
+			mydgemm(&A[(end+1) * n + ib], &A[ib * n + end +1], &A[(end+1) * n + (end + 1)], n , (n - end - 1) , (end-ib+1/*=b*/), 32);
+		}	
+
     return 0;
 }
